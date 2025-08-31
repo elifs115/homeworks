0334d97752eb499733b781cc5b26ae9203fd2f9a
elif sude demir <119362334+elifs115@users.noreply.github.com>
2025-08-31 21:59:17 +0300

    Delete routes directory

--- routes/usersRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAllUsers, addUser, getUserTodos} from '../services/usersService.js';
-import { checkUserFormat } from "../middleware/validateusercreate.js";
-
-const router = Router();
-router.get('/', getAllUsers);
-router.post('/',checkUserFormat, addUser);
-router.get('/:userId/todos', getUserTodos);
-export default router;
